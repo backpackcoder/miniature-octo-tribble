66440161920591d7e5657b4cfdce7b02e51c8a42
Andrew Hammer <backpackcoder@users.noreply.github.com>
2017-02-14 00:21:26 +0300

    Close #15 Add vanilla html form to form-builder (#19)

---
+++ form-builder/vanilla/vanilla.js
@@ -0,0 +1,39 @@
+'use strict'
+
+$(() => {
+    var elChooseLocBtn = $('#btnChooseLoc');
+    var elLocation = $('#inpLocation');
+    var elMap = $('#divMap');
+    var map = L.map('divMap');
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
+
+    elChooseLocBtn.on('click', (ev) => {
+        if (elMap.is(':visible')) {
+            elMap.fadeOut();
+        } else {
+            elMap.fadeIn();
+            var lat = -9.1, lng = 34.0;
+            var coords, marker;
+
+            if (elLocation.val()) {
+                coords = elLocation.val().split(',');
+                if (coords.length === 2) {
+                    lat = parseFloat(coords[0]);
+                    lng = parseFloat(coords[1]);
+                    if (isNaN(lat) || isNaN(lng)) {
+                        lat = -9.1;
+                        lng = 34.0;
+                    }
+                }
+            }
+            map.setView([lat, lng], 8);
+            if (marker) {
+                map.removeLayer(marker);
+            }
+            marker = L.marker([lat, lng], { draggable: true }).addTo(map)
+            marker.on('dragend', (ev) => {
+                elLocation.val(ev.target._latlng.lat + ',' + ev.target._latlng.lng);
+            });
+        }
+    });
+});
