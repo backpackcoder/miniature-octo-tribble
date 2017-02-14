6c2940f7e4e8c39f5bd5b2fcedb396fd3fae9dfc
Andrew Hammer <backpackcoder@users.noreply.github.com>
2017-02-14 15:36:40 +0300

    Close #16 Add bootstrap v3 and v4 froms to form-builder (#21)

---
+++ form-builder/bootstrap4/bootstrap4.js
@@ -0,0 +1,40 @@
+'use strict';
+
+$(function () {
+    var elChooseLocBtn = $('#btnChooseLoc');
+    var elLocation = $('#inpLocation');
+    var elMap = $('#divMap');
+    elMap.height($('.container').height());
+    var map = L.map('divMap');
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
+
+    elChooseLocBtn.on('click', function (ev) {
+        if (elMap.is(':visible')) {
+            elMap.fadeOut();
+        } else {
+            elMap.fadeIn();
+            var lat = -9.1, lng = 34.0;
+            var coords, marker;
+
+            if (elLocation.val()) {
+                coords = elLocation.val().split(',');
+                if (coords.length === 2) {
+                    lat = parseFloat(coords[0]);
+                    lng = parseFloat(coords[1]);
+                    if (isNaN(lat) || isNaN(lng)) {
+                        lat = -9.1;
+                        lng = 34.0;
+                    }
+                }
+            }
+            map.setView([lat, lng], 8);
+            if (marker) {
+                map.removeLayer(marker);
+            }
+            marker = L.marker([lat, lng], { draggable: true }).addTo(map);
+            marker.on('dragend', function (ev) {
+                elLocation.val(ev.target._latlng.lat + ',' + ev.target._latlng.lng);
+            });
+        }
+    });
+});
